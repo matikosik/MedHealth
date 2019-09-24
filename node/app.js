e6fcb745f2cdc1b755508a69adc0b4e27c09241e
matikosik <matiaskosik@gmail.com>
2019-09-24 11:59:42 -0300

    hacer andar

--- node/app.js
+++ node/app.js
@@ -2,22 +2,11 @@ var http = require('http');
 var fs = require('fs');
 
 var server  = http.createServer(function(req, res){
-    console.log('se pidio algo al servidor' + req.url);
+    console.log('se pidio algo al servidor ' + req.url);
     res.writeHead(200, {'Content-Type': 'text/html'});
-    fs.readFile('../index.html', null, function(error, data){
-        if(error)
-        {
-            res.writeHead(404);
-            res.write('No se encuentra el archivo');
-        }
-        else
-        {
-            res.write(data);
-        }
-        res.end();
-    });
-    //myReadStream.pipe(res);
+    fs.createReadStream('../index.html').pipe(res);
 });
 
 server.listen(3000, 'localhost');
 console.log('estoy escuchando a puerto 3000')
+
