9087b0bef86a78783f2a45d2fead8ccae4214444
matikosik <matiaskosik@gmail.com>
2019-09-23 22:36:25 -0300

    23 de septiembre

---
+++ js/MedHealth.js
@@ -0,0 +1,23 @@
+$(document).ready(function(){
+
+    $("#reg").click(function(){
+        alert ("hola");
+    });
+    
+    $("#log").click(function(){
+        alert ("chau");
+    });
+    
+    
+    
+    });
+
+function Registrar()
+{
+    window.location.href = 'HTML/register.html';
+}
+
+function LogIn()
+{
+    window.location.href = 'Login.html';
+}
