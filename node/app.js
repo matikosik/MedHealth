65ce38acdcf0f1cb71c9054a256d12df4128bfab
matikosik <matiaskosik@gmail.com>
2019-09-24 09:50:18 -0300

    nodemon install

--- node/app.js
+++ node/app.js
@@ -1,2 +1,23 @@
-console.log('anda git');
-console.log(__dirname);
+var http = require('http');
+var fs = require('fs');
+
+var server  = http.createServer(function(req, res){
+    console.log('se pidio algo al servidor' + req.url);
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    fs.readFile('../index.html', null, function(error, data){
+        if(error)
+        {
+            res.writeHead(404);
+            res.write('No se encuentra el archivo');
+        }
+        else
+        {
+            res.write(data);
+        }
+        res.end();
+    });
+    //myReadStream.pipe(res);
+});
+
+server.listen(3000, 'localhost');
+console.log('estoy escuchando a puerto 3000')
