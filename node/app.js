b8a24272bb49e7d8760a76d61f8e5db7a0c149ae
matikosik <matiaskosik@gmail.com>
2019-09-24 12:07:17 -0300

    express

--- node/app.js
+++ node/app.js
@@ -1,6 +1,17 @@
 var http = require('http');
 var fs = require('fs');
 
+
+const express = require('express')
+var app = express()
+
+app.get('/', function(req, res){
+    res.send('hello world')
+});
+
+app.listen(3000)
+
+/*
 var server  = http.createServer(function(req, res){
     console.log('se pidio algo al servidor ' + req.url);
     res.writeHead(200, {'Content-Type': 'text/html'});
@@ -9,4 +20,4 @@ var server  = http.createServer(function(req, res){
 
 server.listen(3000, 'localhost');
 console.log('estoy escuchando a puerto 3000')
-
+*/
