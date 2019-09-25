11886e7574bae7ae68546e97ac677861c5d8260d
matikosik <matiaskosik@gmail.com>
2019-09-25 09:57:37 -0300

    Update app.js

--- node/app.js
+++ node/app.js
@@ -1,11 +1,14 @@
 var express = require('express')
 var app = express()
 
+app.use('/', express.static('../'));
+
+/*
 app.use('/css', express.static('../css'));
 app.use('/js', express.static('../js'));
 app.use('/images', express.static('../images'));
 app.use('/html', express.static('../html'));
-
+*/
 
 app.get('/', (req, res) => {
     res.sendFile('index.html', {root: '../'});
