822f460a8ee08b9b539af5a452853a057b189039
matikosik <matiaskosik@gmail.com>
2019-09-27 08:03:55 -0300

    Update app.js

--- node/app.js
+++ node/app.js
@@ -48,14 +48,3 @@ app.post('/register', urlencoderParser, (req, res) => {
 app.listen(3000, () => {
     console.log('estoy escuchando a puerto 3000');
 }); 
-
-/*
-var server  = http.createServer(function(req, res){
-    console.log('se pidio algo al servidor ' + req.url);
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    fs.createReadStream('../index.html').pipe(res);
-});
-
-server.listen(3000, 'localhost');
-console.log('estoy escuchando a puerto 3000')
-*/
