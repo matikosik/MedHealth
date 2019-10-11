7fc015bdf1a59507bea0513d3680c1df1ec0db46
KOSIK MATIAS <44554489@ort>
2019-10-11 10:58:48 -0300

    register

--- node/app.js
+++ node/app.js
@@ -10,9 +10,14 @@ var router = express.Router();
 var urlencoderParser = bodyParser.urlencoded({extended: false});
 
 moongose.connect('mongodb://localhost/MedHealth-mongo', 
-    {
-    useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true}, () => console.log('se conecto la base de datos correctamente'))
-    
+    {useNewUrlParser: true, useUnifiedTopology: true});
+ 
+var db = moongose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
+// SCHEMAS
+var LoginMongo = require(__dirname + '/models/login.js');
+// FIN SCHEMAS
 
 app.set('views', path.join('views'));
 app.set('view engine', 'ejs');
@@ -23,42 +28,49 @@ app.use(morgan('dev'));
 
 
 app.get('/', (req, res) => {
-    res.render('index.ejs');
+    res.render('index');
 });
 
  
 //      LOGIN
 app.get('/login', (req, res) => {
-    res.render('login.ejs', {root: 'views/html'});
+    res.render('login', {root: 'views/html'});
 });
-  // , {root: 'views/html'}
+
 app.post('/login',urlencoderParser, (req, res) => {
-    res.render('login.ejs', {root: 'views/html'});
+    res.render('login', {root: 'views/html'});
     console.log(JSON.stringify(req.body, null, 2));
 });
 //      FIN LOGIN
 
 //      REGISTER
 app.get('/register', (req, res) => {
-    res.render('register.ejs', {root: 'views/html'});
+    res.render('register', {root: 'views/html'});
 });
 
-app.post('/register', urlencoderParser, (req, res) => {
-    res.render('register.ejs', {root: 'views/html'});
-    console.log(JSON.stringify(req.body, null, 2));
+app.post('/register', urlencoderParser, async(req, res) => {
+    res.render('register', {root: 'views/html'});   
+    console.log(req.body);
+    
+    const User = new LoginMongo({
+        name: req.body.name,
+        lastName: req.body.lastName,
+        email: req.body.email,
+        password: req.body.password,
+        rptpassword: req.body.rptpassword,
+    });
+    try{
+        const savedTurista = await User.save();
+        console.log(savedTurista);
+    }
+    catch(error)
+    {
+        console.log("MaxiPete");
+    }
+
 });
 //      FIN REGISTER
 
-
-//  BASE DE DATOS
-
-/*
-var data = [{item: 'anda1'}, {item: 'anda2'}]
-
-app.get('/login', function(req, res){
-    res.sendFile('login', {datas: data});
-});
-*/
 app.listen(3000, () => {
     console.log('estoy escuchando a puerto 3000');
 }); 
---
+++ node/models/login.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const LoginSchema = Schema({
+    name: {
+        type: String
+    },
+    lastName: {
+        type: String
+    },
+    email: {
+        type: String
+    },
+    password: {
+        type: String
+    },
+    rptpassword: {
+        type: String
+    }
+});
+
+module.exports = mongoose.model('login', LoginSchema);
