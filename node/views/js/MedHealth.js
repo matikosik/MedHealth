7af2fd48d89bbaafa93d2039c6001f0e32c1570b
matikosik <50776153+matikosik@users.noreply.github.com>
2019-11-01 14:39:29 -0300

    Register

--- node/views/js/MedHealth.js
+++ node/views/js/MedHealth.js
@@ -21,3 +21,12 @@ function LogIn()
 {
     window.location.href = 'HTML/Login.html';
 }
+
+function avatar() {
+    var x = document.getElementById("avconf");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
